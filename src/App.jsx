55c5e2759290114fbf5748c3822f9025ba6e8d03
EmundoT <etrivelli@regscale.com>
2026-07-14 17:44:46 -0400

    front-door: HEADLINE -- say We Are Open

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import { useState } from 'react'
 import { AUTH_LABEL } from './auth/login-widget.jsx'
 
-const HEADLINE = 'Doors Open v1'
+const HEADLINE = 'We Are Open v2'
 const BASE = 'http://localhost:8788'
 
 export default function App() {
